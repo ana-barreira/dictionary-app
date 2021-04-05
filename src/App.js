0e7e3d2e855066c18033b52e95d30ef35dbdcd3f
ana-barreira <74630055+ana-barreira@users.noreply.github.com>
2021-04-05 21:13:10 +0100

    Added link to repository. Changed button color

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ export default function App() {
       </main>
       < br />
       <footer>
-        Coded by Ana Barreira
+        Coded by <a href="https://github.com/ana-barreira/dictionary-app" target="_blank"> Ana Barreira </a>
       </footer>
       </div>
     </div>
