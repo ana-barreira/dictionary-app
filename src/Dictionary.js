e3906cebf04b65d038f0ea043112b5d775d26dd3
ana-barreira <74630055+ana-barreira@users.noreply.github.com>
2021-04-05 02:51:39 +0100

    Added dictionary API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary(){
   const[keyword, setKeyword]=useState("");
 
+  function handleResponse(response){
+    console.log(response.data[0]);
+  }
+
  function search(event){
 event.preventDefault();
- alert(`${keyword}`);
- }
+ let apiURL=`https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+ axios.get(apiURL).then(handleResponse);}
 
  function handleKeywordChange(event){
 setKeyword (event.target.value);
