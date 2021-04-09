ab0cd391be1f1f83076e7e67495c61bdfbe0c812
ana-barreira <74630055+ana-barreira@users.noreply.github.com>
2021-04-09 17:35:07 +0100

    Added API integration for photos. Final styling.

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,20 +2,34 @@ import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary(props){
   const[keyword, setKeyword]=useState(props.defaultKeyword);
   const[results, setResults]=useState(null);
   const [loaded, setLoaded]=useState(false);
+  const [photos, setPhotos]=useState(null);
+
+   
 
   function handleResponse(response){
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response){
+setPhotos(response.data.photos);
   }
 
   function search (){
 //documentation: https://dictionaryapi.dev/
  let apiURL=`https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
  axios.get(apiURL).then(handleResponse);
+
+let pexelsApiKey="563492ad6f9170000100000184413a23d0bd462ca28e971c684de0a7";
+let pexelsApiUrl=`https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+let headers={ Authorization: `Bearer ${pexelsApiKey}`};
+axios.get (pexelsApiUrl, {headers: headers}).then(handlePexelsResponse);
+
   }
 
  function handleSubmit(event){
@@ -46,6 +60,7 @@ setKeyword (event.target.value);
     </div>
     </section>
     <Results results={results} />
+    <Photos photos={photos} />
     </div>
   );
 } else{
